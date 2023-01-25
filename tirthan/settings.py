c99342673975fee0ed28e76e6951265d2623aa24
tejender <kumartejender007@gmail.com>
2023-01-25 12:48:39 +0530

    added home

--- tirthan/settings.py
+++ tirthan/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
