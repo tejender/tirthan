5aaf513e25897801a7cf19ae44e1fe5325e4fdac
tejender <kumartejender007@gmail.com>
2023-01-31 21:43:04 +0530

    added room section 2

--- base/views.py
+++ base/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 
 def Home(request):
-    active_page='rooms';
-    context={active_page:active_page}
+    active_page='home';
+    context={'active_page':active_page}
     return render(request, 'base/home.html',context)
 
 def Rooms(request):
-    return render(request,'base/rooms.html')
+    active_page='rooms';
+    context={'active_page':active_page}
+    return render(request,'base/rooms.html',context)
