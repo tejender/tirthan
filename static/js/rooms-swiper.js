bac97f479e988ed3ada796cacbf342aa95e31276
tejender <kumartejender007@gmail.com>
2023-02-03 14:07:57 +0530

    room modifies

--- static/js/rooms-swiper.js
+++ static/js/rooms-swiper.js
@@ -1,12 +1,12 @@
 var width = window.innerWidth;
 var views = 3;
 if (width<600){
-    views=1.2
+    views=2
 }
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: views,
-    spaceBetween: 5,
+    spaceBetween: 25,
     loop: true,
     pagination: {
       el: ".swiper-pagination",
