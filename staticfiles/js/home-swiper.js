2692204066639dc403129f2d6ec897f000b55d13
tejender <kumartejender007@gmail.com>
2023-02-08 20:08:58 +0530

    room modifies new

--- staticfiles/js/home-swiper.js
+++ staticfiles/js/home-swiper.js
@@ -1,13 +1,22 @@
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+var width = window.innerWidth;
+if (width<=500){
+  var slideCount = 1
+}
+if(width>500){
+  var slideCount = 3
+}
+
+console.log(width);
+console.log(slideCount);
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: slideCount,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
+});
